415e00f673728e47fe9bd82a92dc02fddb5654d0
gustinedarko <gustine369@gmail.com>
2025-05-29 19:42:47 -0700

    Complete Portfolio website

--- index.js
+++ index.js
@@ -4,20 +4,24 @@ const navTextElements = document.querySelectorAll('.nav-text');
 // detect scroll event on the document
 document.addEventListener('scroll', function () {
     // console.log(window.scrollY);
-    if (window.scrollY > 70) {
+    if (window.scrollY > 150) {
         // change navbar bg to white and text to black
-        navElement.style.backgroundColor = 'white';
+        navElement.classList.add('bg-white');
+        navElement.classList.remove('bg-transparent');
         // Change text color to black
         navTextElements.forEach(function (el) {
-            el.style.color = 'black';
+            el.classList.add('text-black');
+            el.classList.remove('text-white');
     });
  } else {
         // change navbar bg to transparent
-        navElement.style.backgroundColor = 'transparent';
+        navElement.classList.add('bg-transparent');
+        navElement.classList.remove('bg-white');
 
          // Revert text color to white (or your preferred default)
         navTextElements.forEach(function (el) {
-            el.style.color = 'white';
+            el.classList.add('text-white');
+            el.classList.remove('text-black');
         });
     }
 });
